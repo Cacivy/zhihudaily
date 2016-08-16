d9d06b0ead09a0c7a432c03bf6b0aa89e1771d09
cacivy <q609448@live.com>
2016-08-17 07:18:03 +0800

    first

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: process.env.PORT || 8080,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {}
