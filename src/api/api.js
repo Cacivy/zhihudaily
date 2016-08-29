4de492f5feff0de3ffc5a9e30e70ef13ff5e3b70
ZhanTao <q609448@live.com>
2016-08-29 16:54:47 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 //Zhihu http://localhost:3000/GET?host=news-at.zhihu.com&url=/api/4/news/latest
 var config= {
-    site: '104.160.39.244:3000',
+    site: '104.160.39.244:3003',
     host: 'news-at.zhihu.com',
     method: 'GET'
 }
