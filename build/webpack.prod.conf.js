00e51020c4e5a0959674b631d0b928d1a7ea67bd
Cacivy <q609448@live.com>
2016-09-01 16:31:08 +0800

    update favicon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -46,7 +46,6 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
-      favicon:'./favicon.ico',
       template: 'index.html',
       inject: true,
       minify: {
