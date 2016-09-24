7d91cfb98ed693aba252a032beff79a0043e91f5
Cacivy <609448234@qq.com>
2016-09-25 00:59:16 +0800

    去掉异步组件，

--- build/prod-server.js
+++ build/prod-server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var compress = require('compression');
+var port = process.env.PORT || 3000;
 
 app.use(compress());
 app.use('/', express.static('dist'));
 app.use('/static', express.static('static'));
 
-app.listen(3000);
+app.listen(port);
+
+console.log('localhost:' + port);
