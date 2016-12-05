b9d8737d3a1fcb2d9e50fcf19d561adcf20c9a15
Cacivy <609448234@qq.com>
2016-12-05 15:26:23 +0800

    update server

--- src/api/server.js
+++ src/api/server.js
@@ -1,4 +1,5 @@
 //实现一个api请求代理
+//参数 例 http://localhost:3000/GET?host=news-at.zhihu.com&url=/api/4/start-image/1080*1776
 
 var http= require('http')
 var url = require('url');
@@ -9,14 +10,14 @@ console.log('Listening at http://localhost:' + port + '\n')
 var type=['GET','POST'];
 http.createServer(function(req, response){
  response.writeHead(200, {'Content-type' : 'application/json; charset=UTF-8','Access-Control-Allow-Origin':'*'});
- //参数 例 http://localhost:3000/GET?host=news-at.zhihu.com&url=/api/4/start-image/1080*1776
+
  var pathname = url.parse(req.url).pathname.toUpperCase();
  var arg = url.parse(req.url).query;
  var str = qs.parse(arg);
 
  var opt = {
   host: str.host,
-  port:'',
+  port: '80',
   method:pathname.substring(1),
   path: str.url,
   headers:{
@@ -24,22 +25,33 @@ http.createServer(function(req, response){
   }
 }
 
-
-var req = http.request(opt, function(res) {
+var req = http.request(`http://${str.host}${str.url}`, function(res){
   res.setEncoding('utf8');
   var body=''
   res.on('data',function(d){
     body += d
   }).on('end', function(){
-    console.log(opt)
-    console.log(res.headers)
   console.log(body)
-  console.log('------------------------------')
 
-  body.replace(/http\w{0,1}:\/\/pic/g, "https://images.weserv.nl/?url=pic")
   response.write(body)
   response.end()
-});
+})
+
+// http.request(opt, function(res) {
+//   res.setEncoding('utf8');
+//   var body=''
+//   res.on('data',function(d){
+//     body += d
+//   }).on('end', function(){
+//     console.log(opt)
+//     console.log(res.headers)
+//   console.log(body)
+//   console.log('------------------------------')
+
+//   body.replace(/http\w{0,1}:\/\/pic/g, "https://images.weserv.nl/?url=pic")
+//   response.write(body)
+//   response.end()
+// });
 
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
