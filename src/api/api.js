25925bf2cdb5375a514181395307cefb0da6b6d4
Cacivy <609448234@qq.com>
2016-12-05 14:15:44 +0800

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 //Zhihu http://localhost:3000/GET?host=news-at.zhihu.com&url=/api/4/news/latest
 var config= {
-    site: 'localhost:3003',
+    site: 'www.cacivy.com:3003',
     host: 'news-at.zhihu.com',
     method: 'GET'
 }
